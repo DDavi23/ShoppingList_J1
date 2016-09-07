64d6e9c3d982dacba6026ad35d2141c90e804284
Ryan O'Hern <ryanohern@gmail.com>
2016-09-07 13:11:34 -0400

    test post to api from postman working

--- routes/shopping-lists.js
+++ routes/shopping-lists.js
@@ -3,15 +3,16 @@ var mongoUtil = require( '../mongoUtil' );
 
 
 router.post('/', function (req, res){
+	// mongo set up
 	var db = mongoUtil.getDb();
 	db.createCollection('shoppingLists', function(err, collection) {});
 	var shoppingLists = db.collection('shoppingLists');
 
-	var doc = {mykey:1};
-	// shoppingLists.insert(doc, {w:1}, function(err, result) {
-	// 	console.log('you inserted doc');
-	// });
-	res.send(req);
+	var item = req.body;
+	shoppingLists.insert(item, {w:1}, function(err, result) {
+		console.log('you inserted ' + item.name);
+	});
+	res.send(item.name);
 });
 
 

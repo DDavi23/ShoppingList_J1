97349ac0f07187cff6888fe3f82edd1c80e81af9
Ryan O'Hern <ryanohern@gmail.com>
2016-09-06 23:18:51 -0400

    added todos to the app.js file

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+///////// TODO /////////
+
+/*
+1. Connect to a MongoDB (check out different ORM options)
+2. Data Models
+3. Middleware? (check out workout log)
+4. API Routes
+*/
+
 app.listen(5000, function () {
   console.log('app is listening in the upside down on port 5000');
 });

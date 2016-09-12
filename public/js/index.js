cff1ee3463756746c5b32c8e7a9145bfcfd7d4fb
Ryan O'Hern <ryanohern@gmail.com>
2016-09-12 13:55:57 -0400

    moved navbar to header.html with ng-include

--- public/js/index.js
+++ public/js/index.js
@@ -21,6 +21,26 @@ app.config(['$routeProvider', function($routeProvider){
 		});
 }]);
 
+app.controller('headerController', ['$scope', '$http', function($scope, $http){
+	
+	$scope.lists = [];
+
+	$http({
+  		method: 'GET',
+  		url: 'http://localhost:5000/api/shopping-lists'
+	}).then(function successCallback(response) {
+		console.log('it worked');
+		$scope.lists = response.data;
+		console.log($scope.lists);
+  	}, function errorCallback(response) {
+  		console.log('it did not work');
+		console.log(response.statusText);
+  	});
+
+
+}]);
+
+
 app.controller('HomeController', ['$scope', '$http', function($scope, $http){
 	
 	$scope.lists = [];
